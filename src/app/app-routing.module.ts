6a8ead95deca85773fb912d2b67c12864bb52064
AINA SHAHEERA BINTI HAMDAN <ainashaheera66@gmail.com>
2020-03-06 00:08:47 +0800

    authentication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,22 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import('./pages/menu/menu.module').then( m => m.MenuPageModule)
   },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'verify-email',
+    loadChildren: () => import('./verify-email/verify-email.module').then( m => m.VerifyEmailPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./dashboard/dashboard.module').then( m => m.DashboardPageModule)
+  },
 ];
 
 @NgModule({
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  firebaseConfig:
+  {
+    apiKey: "AIzaSyASXXs98RSnERRhiP1bQGWYh2tVHi5GPTc",
+    authDomain: "project-b4e2b.firebaseapp.com",
+    databaseURL: "https://project-b4e2b.firebaseio.com",
+    projectId: "project-b4e2b",
+    storageBucket: "project-b4e2b.appspot.com",
+    messagingSenderId: "198466477841",
+    appId: "1:198466477841:web:1d7543d3780b96bca8b6b0"
+  }
 };
 
 /*
